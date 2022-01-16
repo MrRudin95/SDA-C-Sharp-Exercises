0285da6cb9bfd3fc825db3c6e0dc1c898455aea2
Mieszko <mblaszyk7@gmail.com>
2022-01-16 14:22:13 +0100

    Zakończono zadanie E01

--- exercises/Program.cs
+++ exercises/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿
 
-//Napisz program, który po uruchomieniu wyświetli komunikat "Witaj E00 :-)!".
+/*
 
+Napisz metodę, która będzie wyświetlała prosty cennik danego produktu.
+W zależności od ceny produktu oraz liczby linii, którą ktoś sobie życzy widzieć wydrukuj cennik w
+postaci:
+Liczba sztuk: 1 łączna cena: 5zł
+Liczba sztuk: 2 łączna cena: 10zł
+Liczba sztuk: 3 łączna cena: 15zł
+*/
 
-namespace exercises
+using System;
+
+namespace sda_csharp_exercises
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj E00 :-)!!");
+            int quantity;
+            int price;
+
+            quantity = 3;
+            price = 5;
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.WriteLine("Liczba sztuk: " + i + " łączna cena: " + i * price + "zł");
+            }
         }
     }
 }
